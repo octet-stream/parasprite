30aab260a1b3b0aa398d5ea687fc1307e161175c
Nick K <nick.kruchinin@gmail.com>
2017-03-06 00:19:11 +0300

    Added more tests for Type class

---
+++ test/unit/helper/util/toListTyleIfNeeded.js
@@ -0,0 +1,28 @@
+import test from "ava"
+
+import {GraphQLString, GraphQLList, GraphQLScalarType} from "graphql"
+import isFunction from "lodash.isfunction"
+
+import toListTypeIfNeeded from "helper/util/toListTypeIfNeeded"
+
+test("Should be a function", t => {
+  t.plan(1)
+
+  t.true(isFunction(toListTypeIfNeeded))
+})
+
+test("Should return given type without any changes", t => {
+  t.plan(3)
+
+  const res = toListTypeIfNeeded(GraphQLString)
+
+  t.true(res instanceof GraphQLScalarType)
+  t.is(res.name, GraphQLString.name)
+  t.deepEqual(res, GraphQLString)
+})
+
+test("Should return GraphQLList type when array with type given", t => {
+  t.plan(1)
+
+  t.true(toListTypeIfNeeded([GraphQLString]) instanceof GraphQLList)
+})
---
+++ test/unit/schema/Type.js
@@ -0,0 +1,74 @@
+import test from "ava"
+
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull
+} from "graphql"
+import isFunction from "lodash.isfunction"
+
+import Base from "schema/Base"
+import Type from "schema/Type"
+
+test("Should be a class that extends Base", t => {
+  t.plan(2)
+
+  t.true(isFunction(Type))
+
+  const TSomeType = Type("TSomeType")
+
+  t.true(TSomeType instanceof Base)
+})
+
+test(
+  "Should be return an instance of GraphQLObjectType on \"end\" invocation",
+  t => {
+    t.plan(1)
+
+    const TSomeType = Type("TSomeType").end()
+
+    t.true(TSomeType instanceof GraphQLObjectType)
+  }
+)
+
+test("Should be return a type with specified name and description", t => {
+  t.plan(2)
+
+  const TSomeType = Type("TSomeType", "Some type description").end()
+
+  t.is(TSomeType.name, "TSomeType")
+  t.is(TSomeType.description, "Some type description")
+})
+
+test("Should create a field with given params", t => {
+  const expectedFileds = {
+    someField: {
+      name: "someField",
+      type: GraphQLString,
+      description: null,
+      deprecationReason: undefined,
+      isDeprecated: false,
+      args: []
+    }
+  }
+
+  const TSomeType = Type("TMyObjectType")
+      .field("someField", GraphQLString, false)
+    .end()
+
+  const fields = TSomeType.getFields()
+
+  t.deepEqual(fields, expectedFileds)
+})
+
+test("Should mark type as non-null when \"required\" parameter is true", t => {
+  t.plan(1)
+
+  const TSomeType = Type("TSomeType", "Some type description")
+      .field("someField", GraphQLString, true)
+    .end()
+
+  const {someField} = TSomeType.getFields()
+
+  t.true(someField.type instanceof GraphQLNonNull)
+})
