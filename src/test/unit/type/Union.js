d69f161f507d679b908be9419dac763ea32dc8f1
Nick K <nick.kruchinin@gmail.com>
2017-11-29 13:00:52 +0300

    Improve tests for Unit type.

--- src/test/unit/type/Union.js
+++ src/test/unit/type/Union.js
@@ -21,6 +21,42 @@ const DATA_SOURCE = [
   }
 ]
 
+const TBook = Type("Book")
+  .field({
+    name: "author",
+    type: TString,
+    required: true
+  })
+  .field({
+    name: "title",
+    type: TString,
+    required: true
+  })
+  .field({
+    name: "pages",
+    type: TInt,
+    required: true
+  })
+.end()
+
+const TMovie = Type("Movie")
+  .field({
+    name: "director",
+    type: TString,
+    required: true
+  })
+  .field({
+    name: "title",
+    type: TString,
+    required: true
+  })
+  .field({
+    name: "runningTime",
+    type: TInt,
+    required: true
+  })
+.end()
+
 const handler = (_, {name}) => (
   DATA_SOURCE.find(({author, director, username}) => (
     [author, director, username].includes(name)
@@ -30,14 +66,6 @@ const handler = (_, {name}) => (
 test("Should return a GraphQLUnionType", t => {
   t.plan(1)
 
-  const TBook = Type("Book")
-    .field({
-      name: "author",
-      type: TString,
-      required: true
-    })
-  .end()
-
   const TSearchable = Union("Searchable", [TBook])
     .match(({author}) => author && TBook)
   .end()
@@ -45,26 +73,26 @@ test("Should return a GraphQLUnionType", t => {
   t.true(TSearchable instanceof GraphQLUnionType)
 })
 
-test("Should correctly resolve a type", async t => {
-  const TBook = Type("Book")
-    .field({
-      name: "author",
-      type: TString,
-      required: true
-    })
-    .field({
-      name: "title",
-      type: TString,
-      required: true
-    })
-    .field({
-      name: "pages",
-      type: TInt,
-      required: true
-    })
-  .end()
+test(
+  "Should correctly create a GraphQLUnionType when constructor takes an object",
+  t => {
+    t.plan(1)
 
-  const TSearchable = Union("Searchable", [TBook])
+    const TSearchable = Union({
+      name: "Searchable",
+      description: "Represents a union searchable type.",
+      types: [TBook]
+    })
+      .match(({author}) => author && TBook)
+    .end()
+
+    t.true(TSearchable instanceof GraphQLUnionType)
+  }
+)
+
+test("Should correctly resolve a type", async t => {
+  const TSearchable = Union("Searchable", [TBook, TMovie])
+    .match(({director, runningTime}) => (director && runningTime) && TMovie)
     .match(({author, pages}) => (author && pages) && TBook)
   .end()
 
