25f607962c05a3b40c92d1457647c860dbb3a6d5
Nick K <nick.kruchinin@gmail.com>
2019-03-01 03:27:38 +0300

    Update engine requirements.

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    "@babel/proposal-export-default-from",
+    "@babel/proposal-export-namespace-from",
     "@babel/transform-async-to-generator",
     ["@babel/transform-modules-commonjs", {
       mjsStrictNamespace: false
